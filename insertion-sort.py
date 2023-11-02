058059ff39c2d8d707fbc53885ad98d56fb6292d
Émery Freitas Moriconi <emerymoriconi09@gmail.com>
2023-11-02 20:22:08 -0300

    InsertionSort em Python

---
+++ insertion-sort.py
@@ -0,0 +1,20 @@
+# Complexidade: O(N²) - Pior Caso
+# Número de operações: N*(N-1)/2
+
+def insertion(vetor):
+    n = len(vetor)
+    for i in range (1, n):
+        j=i
+        while j > 0:
+            if vetor[j] < vetor[j-1]:
+                aux = vetor[j]
+                vetor[j] = vetor[j-1]
+                vetor[j-1] = aux
+            
+            j=j-1
+
+    return vetor
+
+vetor = [0, 1, 2, 3, 4, 5]
+resultado = insertion(vetor)
+print(resultado)
