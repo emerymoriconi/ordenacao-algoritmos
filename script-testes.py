3127fe625122df040873ac2de9a9a805aa1b97d1
Émery Freitas Moriconi <emerymoriconi09@gmail.com>
2023-11-20 22:28:33 -0300

    Update script-testes.py

--- script-testes.py
+++ script-testes.py
@@ -6,13 +6,10 @@ seed = 42
 random.seed(seed)
 
 #vetor decrescente de 1000 elementos (ajustar a quantidade de acordo com a necessidade)
-vetor_decrescente = sorted(random.sample(range(1, 1000000), 10), reverse=True)
+vetor_decrescente = sorted(random.sample(range(1, 1000000), 1000), reverse=True)
 
 #vetor aleatório de 20 elementos (ajustar a quantidade de acordo com a necessidade)
 vetor_aleatorio = random.sample(range(1, 1000000), 20)
 
 #vetor crescente de 1000 elementos (ajustar a quantidade de acordo com a necessidade)
 vetor_crescente = sorted(random.sample(range(1, 1000000), 1000), reverse=False)
-
-print(vetor_decrescente)
-print(quick_sort.quicksort(vetor_decrescente))
