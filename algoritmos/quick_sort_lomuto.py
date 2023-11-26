12a1e954b464e264ef8837cf595e4c87db8fe302
Émery Freitas Moriconi <emerymoriconi09@gmail.com>
2023-11-26 14:44:34 -0300

    Partição com Random

--- algoritmos/quick_sort_lomuto.py
+++ algoritmos/quick_sort_lomuto.py
@@ -4,17 +4,25 @@
 #N*logN - Melhor Caso. Escolha do pivot que divide o array em dois, ambos com N/2 elementos.
 #O melhor caso, no quicksort, é o predominante.
 #Considerado o algortimo de ordenação mais rápido.
+import random 
 
 def quicksort_lomuto(A, inicio=0, fim=None):
     if fim is None:
         fim = len(A) - 1
     if inicio < fim:
         #p recebe a posição correta do pivot
-        p = partition(A, inicio, fim)
+        p = randomized_partition(A, inicio, fim)
         #divisão e conquista
         quicksort_lomuto(A, inicio, p-1)
         quicksort_lomuto(A, p+1, fim)
     return A
+
+def randomized_partition(A, p, r):
+    #pivô é escolhido aleatoriamente entre p e r (exlcuindo r)
+    aleatorio = random.randrange(p, r)
+    #o último elemento recebeu um número aleatório do vetor, para não cair no pior caso em vetores decrescentes 
+    A[aleatorio], A[r] = A[r], A[aleatorio]
+    return partition(A, p, r)
 
 def partition(A, inicio, fim):
     pivot = A[fim]
--- script-testes.py
+++ script-testes.py
@@ -6,13 +6,13 @@ seed = 42
 random.seed(seed)
 
 #vetor decrescente de 1000 elementos (ajustar a quantidade de acordo com a necessidade)
-vetor_decrescente = sorted(random.sample(range(1, 100), 20), reverse=True)
+vetor_decrescente = sorted(random.sample(range(1, 1000000), 1000), reverse=True)
 
 #vetor aleatório de 20 elementos (ajustar a quantidade de acordo com a necessidade)
-vetor_aleatorio = random.sample(range(1, 1000), 20)
+vetor_aleatorio = random.sample(range(1, 100), 20)
 
 #vetor crescente de 1000 elementos (ajustar a quantidade de acordo com a necessidade)
 vetor_crescente = sorted(random.sample(range(1, 1000000), 1000), reverse=False)
 
-print(vetor_decrescente)
-print(quick_sort_original.quicksort_original(vetor_decrescente))
+print(vetor_aleatorio)
+print(quick_sort_lomuto.quicksort_lomuto(vetor_aleatorio))
